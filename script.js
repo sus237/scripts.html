61490b843eb5114d87efdbffe9fbd6b4c894b11b
sus237 <mangophoneno22@gmail.com>
2025-04-12 17:55:58 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function copyText() {
+  navigator.clipboard.writeText("You copied this ✨");
+  const toast = document.getElementById('toast');
+  toast.classList.add('show');
+  setTimeout(() => {
+    toast.classList.remove('show');
+  }, 3000);
+}
